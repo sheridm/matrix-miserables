3594a1ae75e07137fd587806c54c09c1253cb698
sheridm <masheridan1970@gmail.com>
2022-04-12 11:12:17 +0100

    working mouse followeer

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
-import { Face } from './Face';
-// console.log(BackgroundCircle);
 
 const width = 960;
 const height = 500;
+const circleRadius = 30;
+const initialMousePosition = { x: width / 2, y: height / 2 };
 
-const App = () => (
-    <Face
-        width={width}
-        height={height}
-        centerX={width / 2}
-        centerY={height / 2}
-        strokeWidth={10}
-        eyeOffsetX={90}
-        eyeOffsetY={90}
-        eyeRadius={50}
-        mouthRadius={200}
-        mouthWidth={20}
-    />
-
-);
+const App = () => {
+    const [mousePosition, setMousePosition] = useState(initialMousePosition);
+    const handleMouseMove = useCallback(event => {
+        const { clientX, clientY } = event;
+        setMousePosition({ x: clientX, y: clientY });
+    }, [setMousePosition]);
+    return (
+        <svg width={width} height={height} onMouseMove={handleMouseMove}>
+            <circle
+                cx={mousePosition.x}
+                cy={mousePosition.y}
+                r={circleRadius}
+            />
+        </svg>
+    )
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- static/js/bundle.js
+++ static/js/bundle.js
@@ -1,4 +1,4 @@
-(function (React, ReactDOM, d3) {
+(function (React, ReactDOM) {
     'use strict';
 
     function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
@@ -6,99 +6,39 @@
     var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
     var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
 
-    const BackgroundCircle = ({
-      radius,
-      strokeWidth
-    }) => /*#__PURE__*/React__default["default"].createElement("circle", {
-      r: radius,
-      fill: "yellow",
-      stroke: "black",
-      "stroke-width": strokeWidth
-    });
-
-    const Eyes = ({
-      eyeOffsetX,
-      eyeOffsetY,
-      eyeRadius
-    }) => /*#__PURE__*/React__default["default"].createElement(React__default["default"].Fragment, null, /*#__PURE__*/React__default["default"].createElement("circle", {
-      cx: -eyeOffsetX,
-      cy: -eyeOffsetY,
-      r: eyeRadius
-    }), /*#__PURE__*/React__default["default"].createElement("circle", {
-      cx: +eyeOffsetX,
-      cy: -eyeOffsetY,
-      r: eyeRadius
-    }));
-
-    const Mouth = ({
-      mouthRadius,
-      mouthWidth
-    }) => {
-      const mouthArc = d3.arc().innerRadius(mouthRadius).outerRadius(mouthRadius + mouthWidth).startAngle(Math.PI / 2).endAngle(Math.PI * 3 / 2);
-      return /*#__PURE__*/React__default["default"].createElement("path", {
-        d: mouthArc()
-      });
-    };
-
-    const FaceContainer = ({
-      children,
-      width,
-      height,
-      centerX,
-      centerY
-    }) => /*#__PURE__*/React__default["default"].createElement("svg", {
-      width: width,
-      height: height
-    }, /*#__PURE__*/React__default["default"].createElement("g", {
-      transform: `translate(${centerX}, ${centerY})`
-    }, children));
-
-    const Face = ({
-      width,
-      height,
-      centerX,
-      centerY,
-      strokeWidth,
-      eyeOffsetX,
-      eyeOffsetY,
-      eyeRadius,
-      mouthRadius,
-      mouthWidth
-    }) => /*#__PURE__*/React__default["default"].createElement(FaceContainer, {
-      width: width,
-      height: height,
-      centerX: centerX,
-      centerY: centerY
-    }, /*#__PURE__*/React__default["default"].createElement(BackgroundCircle, {
-      radius: centerY - strokeWidth / 2,
-      strokeWidth: strokeWidth
-    }), /*#__PURE__*/React__default["default"].createElement(Eyes, {
-      eyeOffsetX: eyeOffsetX,
-      eyeOffsetY: eyeOffsetY,
-      eyeRadius: eyeRadius
-    }), /*#__PURE__*/React__default["default"].createElement(Mouth, {
-      mouthRadius: mouthRadius,
-      mouthWidth: mouthWidth
-    }));
-
     const width = 960;
     const height = 500;
+    const circleRadius = 30;
+    const initialMousePosition = {
+      x: width / 2,
+      y: height / 2
+    };
 
-    const App = () => /*#__PURE__*/React__default["default"].createElement(Face, {
-      width: width,
-      height: height,
-      centerX: width / 2,
-      centerY: height / 2,
-      strokeWidth: 10,
-      eyeOffsetX: 90,
-      eyeOffsetY: 90,
-      eyeRadius: 50,
-      mouthRadius: 200,
-      mouthWidth: 20
-    });
+    const App = () => {
+      const [mousePosition, setMousePosition] = React.useState(initialMousePosition);
+      const handleMouseMove = React.useCallback(event => {
+        const {
+          clientX,
+          clientY
+        } = event;
+        setMousePosition({
+          x: clientX,
+          y: clientY
+        });
+      }, [setMousePosition]);
+      return /*#__PURE__*/React__default["default"].createElement("svg", {
+        width: width,
+        height: height,
+        onMouseMove: handleMouseMove
+      }, /*#__PURE__*/React__default["default"].createElement("circle", {
+        cx: mousePosition.x,
+        cy: mousePosition.y,
+        r: circleRadius
+      }));
+    };
 
     const rootElement = document.getElementById("root");
     ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(App, null), rootElement);
 
-})(React, ReactDOM, d3);
+})(React, ReactDOM);
 //# sourceMappingURL=bundle.js.map
